de98290c31ae946d998fa183682e4aa8fc67b9bf
Can Okay <canokay2014@gmail.com>
2018-03-16 21:50:08 +0300

    Iki Sayının Karesi Algoritması C#
    Iki Sayının Karesi Algoritması C# Windows Form Application

---
+++ 1- Iki-Sayinin-Karesi/IkiSayininKaresi-C-Sharp/IkiSayininKaresi-C-Sharp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IkiSayininKaresi_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = Convert.ToInt32(textBox1.Text);
+            label1.Text= Convert.ToString(sayi * sayi);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //int SAYI
+            if ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57)
+            {
+                e.Handled = false;
+            }
+            else if ((int)e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
