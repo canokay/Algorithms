15ace9e42540bc17eab9bced9a44eab5cd9876a8
Can Okay <canokay2014@gmail.com>
2018-05-17 20:55:52 +0300

    Circle Area and Circumference Algorithm
    Python Circle Area and Circumference Algorithm

---
+++ 8-Circle-Area-and-Circumference/Python/Circle-Area-and-Circumference.py
@@ -0,0 +1,7 @@
+pi = 3.14159
+cap = input("Dairenin çapını girin: ")
+yaricap = float(cap) / 2
+alan = pi * (yaricap * yaricap)
+cevre = (2 * pi) * yaricap
+print("Dairenin Alanı: ", alan)
+print("Dairenin Çevresi: ", cevre)
